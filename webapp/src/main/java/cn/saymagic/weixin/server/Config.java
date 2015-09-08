1a6b126c592f424e9121bd709fc7a88dcbdc7930
sirxun <w415944895@sina.com>
2015-09-08 14:37:44 +0800

    Update Config.java
    增加图灵key和微信token

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "thisismytoken";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "932d44ff8779b248b37e71656058c34d"; 
 	
 }
