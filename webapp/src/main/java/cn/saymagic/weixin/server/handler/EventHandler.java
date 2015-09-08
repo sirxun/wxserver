d2d26bd5b2a9227d038b3e11ce26fc8094b75c7f
sirxun <w415944895@sina.com>
2015-09-08 15:15:42 +0800

    Update EventHandler.java
    修改进入微信提示信息

--- webapp/src/main/java/cn/saymagic/weixin/server/handler/EventHandler.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/handler/EventHandler.java
@@ -10,7 +10,7 @@ public class EventHandler extends BaseHandler {
 			return null;
 		else
 			if("subscribe".equals(msgRequest.getEvent())){
-				return getResponseStringByContent("欢迎您关注瑰族，我将定期为您推荐一些经典美食、美食心得、美食做法。要知道，吃，是一种情怀。欢迎您加入吃货的世界。", msgRequest);
+				return getResponseStringByContent("你好~我是咩咩咩~我能跟你聊会天嘛~", msgRequest);
 			}else if("CLICK".equals(msgRequest.getEvent())){
 				String eventKey = msgRequest.getEventKey();
 				String content = "";
@@ -18,7 +18,7 @@ public class EventHandler extends BaseHandler {
 				return getResponseStringByContent(content, msgRequest);
 			}
 			else{ 
-				return getResponseStringByContent("暂时无法处您的请求，请稍后再试。", msgRequest);
+				return getResponseStringByContent("咩咩咩正在睡觉，请等他睡醒哟~", msgRequest);
 			}
 
 	}
